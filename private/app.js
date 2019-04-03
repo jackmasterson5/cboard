5c5e60bc142435785aeb2a817631794a1a343f4d
Jack Masterson <jack.masterson@emxdigital.com>
2019-04-02 22:33:53 -0400

    cleanup

--- private/app.js
+++ private/app.js
@@ -3,19 +3,10 @@ const user = require('./services/users');
 const sequelize = require('./connection');
 
 db.connect(sequelize)
-  .then(() => {
-    return user.init(sequelize);
-  })
-  .then(() => {
-    return user.sync();
-  })
-  .then(() => {
-    return user.create({ firstName: 'Jack', lastName: 'Masterson' })
-  })
-  .then(name => {
-    console.log(`${name}'s auto-generated ID:`, name.id);
-    return db.close(sequelize);
-  })
+  .then(() => user.init(sequelize))
+  .then(() => user.sync())
+  .then(() => user.create({ firstName: 'Jack', lastName: 'Masterson' }))
+  .then(() => db.close(sequelize))
   .catch(err => {
     console.error('Error: ', err);
   });
